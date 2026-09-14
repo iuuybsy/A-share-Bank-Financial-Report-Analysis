ff2589095ea737ccdda3446582ab7a02200f0a97
iuuybsy <18803722821@163.com>
2026-09-14 21:19:21 +0800

    Modify plot logic to compare differernt banks.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
 from data_extract import get_dataframe
@@ -9,34 +8,61 @@ def plot_data(data_name, datas, labels, title):
         ax.plot(data['Year'].values, data[data_name].values, label=labels[i], marker='o')
     ax.set_xlabel("years")
     ax.set_title(title)
+    ax.grid(True)
     plt.legend()
 
-# Convert the file and save the CSV
-df_cmb = get_dataframe("600036_CMB/600036_CMB.xlsx", "600036_CMB/cmb_financials.csv")
-df_cib = get_dataframe("601166_CIB/601166_CIB.xlsx", "601166_CIB/cib_financials.csv")
-df_cncb = get_dataframe("601998_CNCB/601998_CNCB.xlsx", "601998_CNCB/cncb_financials.csv")
+def main():
+    # ----- data extract and prepare -----
+    df_cmb = get_dataframe("600036_CMB/600036_CMB.xlsx", "600036_CMB/cmb_financials.csv")
+    df_cib = get_dataframe("601166_CIB/601166_CIB.xlsx", "601166_CIB/cib_financials.csv")
+    df_cncb = get_dataframe("601998_CNCB/601998_CNCB.xlsx", "601998_CNCB/cncb_financials.csv")
 
-data = [df_cmb, df_cib, df_cncb]
-labels = ["CMB", "CIB", "CNCB"]
+    data = [df_cmb, df_cib, df_cncb]
+    labels = ["CMB", "CIB", "CNCB"]
 
-plot_data('ROA', data, labels, 'ROA')
-plot_data('NonIIRatio', data, labels, 'NonIIRatio')
-plot_data('RORWA', data, labels, 'RORWA')
-plot_data('TotalDeposits', data, labels, 'TotalDeposits')
+    # ----- income and profit -----
+    # plot_data("OperatingIncome", data, labels, "Operating Income")
+    # plot_data("CoreNetProfit", data, labels, "Core Net Profit")
+    # plot_data("DilutedEPS", data, labels, "Diluted EPS")
+    # plot_data("CoreEPS", data, labels, "CoreEPS")
+    plot_data("ROA", data, labels, "ROA(%)")
+    plot_data("CoreROE", data, labels, "Core ROE(%)")
+    plot_data("NIS", data, labels, "NIS(%)")
+    plot_data("NIM", data, labels, "NIM(%)")
+    plot_data("RORWA", data, labels, "RORWA(%)")
+    plot_data("NonIIRatio", data, labels, "Non-Interest Income Ratio(%)")
+    plot_data("CostIncomeRatio", data, labels, "Cost Income Ratio(%)")
 
-fig, axes = plt.subplots(4, 1)
-deposit_ratio_names = ['CorpDemandRatio', 'CorpTimeRatio', 'RetailDemandRatio', 'RetailTimeRatio']
-for i, ax in enumerate(axes):
-    for j, cur_data in enumerate(data):
-        ax.plot(cur_data['Year'].values, cur_data[deposit_ratio_names[i]].values, label=labels[j], marker='o')
-    ax.set_title(deposit_ratio_names[i])
-    ax.legend()
-plt.tight_layout()
+    # ----- assets structure -----
+    # plot_data("TotalAssets", data, labels, "Total Assets")
+    # plot_data("TotalLiabilities", data, labels, "Total Liabilities")
+    # plot_data("Equity", data, labels, "Equity")
+    # plot_data("NAVPS", data, labels, "NAVPS")
+    # plot_data("TotalCapital", data, labels, "Total Capital")
+    # plot_data("CoreTier1Capital", data, labels, "Core Tier 1 Capital")
+    plot_data("CET1", data, labels, "CET1 CAR(%)")
+    plot_data("Tier1CAR", data, labels, "Tier 1 CAR(%)")
+    plot_data("CAR", data, labels, "Capital Adequacy Ratio(%)")
 
-plot_data('ProvisionCoverage', data, labels, 'ProvisionCoverage')
-plot_data('NIS', data, labels, 'NIS')
-plot_data('NIM', data, labels, 'NIM')
+    # ----- deposit structure -----
+    # plot_data("TotalDeposits", data, labels, "Total Deposits")
+    plot_data("CorpDemandRatio", data, labels, "Corp Demand Deposit Ratio(%)")
+    plot_data("CorpTimeRatio", data, labels, "Corp Time Deposit Ratio(%)")
+    plot_data("RetailDemandRatio", data, labels, "Retail Demand Deposit Ratio(%)")
+    plot_data("RetailTimeRatio", data, labels, "Retail Time Deposit Ratio(%)")
 
-plt.show()
+    # ----- loan structure -----
+    # plot_data("GrossLoans", data, labels, "Gross Loans")
+    # plot_data("NPL", data, labels, "NPL")
+    plot_data("NPLRatio", data, labels, "NPL Ratio(%)")
+    plot_data("ProvisionCoverage", data, labels, "Provision Coverage(%)")
+    # plot_data("LoanLossReserve", data, labels, "Loan Loss Reserve")
+    plot_data("NormalMigRate", data, labels, "Normal Mig Rate(%)")
+    plot_data("SpecialMigRate", data, labels, "Special Mig Rate(%)")
+    plot_data("SubstandardMigRate", data, labels, "Substandard Mig Rate(%)")
+    plot_data("DoubtfulMigRate", data, labels, "Doubtful Mig Rate(%)")
 
+    plt.show()
 
+if __name__ == "__main__":
+    main()
